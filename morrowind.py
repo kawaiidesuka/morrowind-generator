9be0c2822e68d4804189dc87b084352ab887afd0
kawaii <nyaa@openmailbox.org>
2016-01-07 15:02:26 -0500

    actually works now

--- morrowind.py
+++ morrowind.py
@@ -1,3 +1,4 @@
+from random import choice
 from random import sample
 from random import randint
 attributenum = randint(0,7)
@@ -8,7 +9,20 @@ signs = ["tower", "shadow", "lover", "ritual", "atronach", "apprentice", "lord",
 skills = ["heavy armour", "medium armour", "spear", "acrobatics", "armourer", "axe", "blunt", "long blade", "block", "light armour", "marksman", "sneak", "athletics", "hand-to-hand", "short blade", "unarmoured", "illusion", "mercantile", "speechcraft", "alchemy", "conjuration", "enchant", "security", "alteration", "destruction", "mysticism", "restoration"]
 races = ["altmer", "argonian", "bosmer", "breton", "dunmer", "imperial", "khajiit", "nord", "orc", "redguard"]
 attributes = ["agility", "endurance", "intelligence", "luck", "personality", "speed", "strength", "willpower"]
-print races[racenum]
-print signs[signnum]
-print sample(skills, 10)
-print sample(attributes, 2)
+print "your race is " + races[racenum]
+print "your birthsign is " + signs[signnum]
+skillcalc = 0
+attributecalc = 0
+print "your major skills are as follows:"
+for skillcalc in range(0,5):
+	print choice(skills)
+	skillcalc + 1
+print "your minor skills are as follows:"
+skillcalc = 0
+for skillcalc in range(0,5):
+	print choice(skills)
+	skillcalc + 1
+print "your primary attributes are as follows:"
+for attributecalc in range(0,2):
+	print choice(attributes)
+	attributecalc + 1
