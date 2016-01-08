f83134daeeb385be6c95c5a3aab16b93b99499eb
Louis Taylor <louis@kragniz.eu>
2016-01-08 03:00:16 +0000

    Fix python 3 problems
    Baka

--- morrowind.py
+++ morrowind.py
@@ -9,20 +9,20 @@ signs = ["tower", "shadow", "lover", "ritual", "atronach", "apprentice", "lord",
 skills = ["heavy armour", "medium armour", "spear", "acrobatics", "armourer", "axe", "blunt", "long blade", "block", "light armour", "marksman", "sneak", "athletics", "hand-to-hand", "short blade", "unarmoured", "illusion", "mercantile", "speechcraft", "alchemy", "conjuration", "enchant", "security", "alteration", "destruction", "mysticism", "restoration"]
 races = ["altmer", "argonian", "bosmer", "breton", "dunmer", "imperial", "khajiit", "nord", "orc", "redguard"]
 attributes = ["agility", "endurance", "intelligence", "luck", "personality", "speed", "strength", "willpower"]
-print "your race is " + races[racenum]
-print "your birthsign is " + signs[signnum]
+print("your race is " + races[racenum])
+print("your birthsign is " + signs[signnum])
 skillcalc = 0
 attributecalc = 0
-print "your major skills are as follows:"
+print("your major skills are as follows:")
 for skillcalc in range(0,5):
-	print choice(skills)
+	print(choice(skills))
 	skillcalc + 1
-print "your minor skills are as follows:"
+print("your minor skills are as follows:")
 skillcalc = 0
 for skillcalc in range(0,5):
-	print choice(skills)
+	print(choice(skills))
 	skillcalc + 1
-print "your primary attributes are as follows:"
+print("your primary attributes are as follows:")
 for attributecalc in range(0,2):
-	print choice(attributes)
+	print(choice(attributes))
 	attributecalc + 1
